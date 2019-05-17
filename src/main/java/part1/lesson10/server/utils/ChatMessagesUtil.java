1582668b16898b139925d6372ca85372e2b71843
Анна Шеронова <shean@1c.ru>
2019-05-17 18:03:46 +0300

    lesson10

---
+++ src/main/java/part1/lesson10/server/utils/ChatMessagesUtil.java
@@ -0,0 +1,14 @@
+package part1.lesson10.server.utils;
+
+public class ChatMessagesUtil {
+    public static final String NEW_USER_MESSAGE = "В чат вошел %s.";
+    public static final String ENTER_MESSAGE = "Для входа в чат введите ваше имя: ";
+    public static final String WELCOME_MESSAGE = "Добро пожаловать, %s!";
+    public static final String QUIT_MESSAGE = "quit";
+    public static final String FROM_PRIVATE_MESSAGE = "Личное сообщение от %s: ";
+    public static final String TO_FROM_PRIVATE_MESSAGE = "Личное сообщение для %s от %s: ";
+    public static final String PRIVATE_BEGIN_SIGNAL = "**";
+    public static final String YOU_LEFT_CHAT = "Вы вышли из чата.";
+    public static final String USER_LEFT_CHAT = "%s вышел из чата.";
+    public static final String ENTER_WRONG_NAME = "Вы ввели неверное имя. Данного пользователя нет в чате.";
+}
