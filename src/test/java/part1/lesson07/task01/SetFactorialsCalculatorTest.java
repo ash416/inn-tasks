259a171477541f05fedf461abea06280ef6f8393
ash <ann.sheronova@gmail.com>
2019-05-19 19:31:45 +0300

    Добавлен тест, демонстрирующий корректную работу программы на больших рандомных данных, тест на время

--- src/test/java/part1/lesson07/task01/SetFactorialsCalculatorTest.java
+++ src/test/java/part1/lesson07/task01/SetFactorialsCalculatorTest.java
@@ -1,5 +1,6 @@
 package part1.lesson07.task01;
 
+import com.google.common.math.BigIntegerMath;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.lang.String.format;
 
 public class SetFactorialsCalculatorTest {
     @Test
@@ -29,5 +34,43 @@ public class SetFactorialsCalculatorTest {
         }
         Assertions.assertThat(result).isEqualTo(expectedFactorials);
     }
+
+    @Test
+    public void calculateWithoutMistakes() throws ExecutionException, InterruptedException {
+        final int countNumbers = 10000;
+        List<Integer> numbers = Stream
+                .generate(() -> (int) (10000 * Math.random()))
+                .limit(countNumbers)
+                .collect(Collectors.toList());
+        SetFactorialsCalculator calculator = new SetFactorialsCalculator(10);
+        List<BigInteger> result = calculator.calculate(numbers);
+        List<BigInteger> expectedResult = numbers.stream()
+                .map(BigIntegerMath::factorial)
+                .collect(Collectors.toList());
+        Assertions.assertThat(result).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void timeTest() throws ExecutionException, InterruptedException {
+        final int countNumbers = 1000;
+        List<Integer> numbers = Stream
+                .generate(() -> (int) (10000 * Math.random()))
+                .limit(countNumbers)
+                .collect(Collectors.toList());
+        printTime(1, numbers);
+        printTime(2, numbers);
+        printTime(4, numbers);
+        printTime(8, numbers);
+        printTime(16, numbers);
+
+    }
+
+    private void printTime(int countThreads, List<Integer> numbers) throws ExecutionException, InterruptedException {
+        String message = "Количество потоков: %s; время: %s";
+        long time = System.currentTimeMillis();
+        new SetFactorialsCalculator(countThreads).calculate(numbers);
+        System.out.println(format(message, countThreads, System.currentTimeMillis() - time));
+    }
+
 
 }
